eb07687d0939090bd916c3ec99d567c1107b3678
panda102 <1565675791@qq.com>
2018-01-09 00:19:25 +0800

    修改布局

--- app/src/main/java/com/nxm/muzi102/utils/UITitleLayout.java
+++ app/src/main/java/com/nxm/muzi102/utils/UITitleLayout.java
@@ -1,44 +1,50 @@
-package com.nxm.muzi102.comment;
+package com.nxm.muzi102.utils;
 
+import android.app.ActionBar;
 import android.app.Activity;
 import android.content.Context;
 import android.content.res.Resources;
 import android.graphics.Color;
 import android.os.Build;
+import android.support.annotation.RequiresApi;
 import android.util.AttributeSet;
 import android.view.View;
 import android.view.Window;
-import android.view.WindowManager;
 import android.widget.LinearLayout;
 
 import com.nxm.muzi102.R;
-
+import com.nxm.muzi102.comment.Constants;
 
 /**
  * ******************************************************************************************************************
- * 修改日期                          修改人             任务名称                 功能或Bug描述
- * 2017年12月18日22:05:23            xmz                                      沉浸标题栏布局容器
- *
+ * 修改日期                         修改人             任务名称                 功能或Bug描述
+ * 2018年1月8日22:46:20             LZX                                      沉浸标题栏布局容器
+ * ******************************************************************************************************************
  */
 public class UITitleLayout extends LinearLayout {
     private Context context;
-    public static int bg_color = R.color.common_gray;
-    public static String BG_COLOR = "#D3D5DA";
+    public static int bg_color = R.color.main_theme;
+    public static String BG_COLOR = "#0066ff";
+
+    @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
     public UITitleLayout(Context context) {
         super(context);
         init(context);
     }
 
+    @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
     public UITitleLayout(Context context, AttributeSet attrs) {
         super(context, attrs);
         init(context);
     }
 
+    @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
     private void init(Context context) {
         this.context = context;
         initSystemBar();
-        createContentView();
+//        createContentView();
     }
+
     /**
      * 创建标题控件的布局内容
      */
@@ -53,8 +59,8 @@ public class UITitleLayout extends LinearLayout {
         LayoutParams layoutParams = new LayoutParams(w, h);
         vStatusBar.setLayoutParams(layoutParams);
         addView(vStatusBar, Constants.ZERO);
-
     }
+
     /**
      * 获取状态栏的高度
      */
@@ -63,20 +69,26 @@ public class UITitleLayout extends LinearLayout {
             return Constants.ZERO;
         }
         Resources res = getResources();
-        int resourceId = res.getIdentifier("status_bar_height","dimen", "android");
+        int resourceId = res.getIdentifier("status_bar_height", "dimen", "android");
         if (resourceId > 0) {
             return res.getDimensionPixelSize(resourceId);
         }
         return Constants.ZERO;
     }
+
     /**
      * 设置沉浸式状态栏
      */
+    @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
     private void initSystemBar() {
         // 判断当前SDK版本号，如果是4.4以上，才支持沉浸式状态栏
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
-            Window window = ((Activity) context).getWindow();
-            window.addFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
+            View decorView = ((Activity) context).getWindow().getDecorView();
+            //让应用主题内容占用系统状态栏的空间,注意:下面两个参数必须一起使用 stable 牢固的
+            int option = View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN | View.SYSTEM_UI_FLAG_LAYOUT_STABLE;
+            decorView.setSystemUiVisibility(option);
+            //设置状态栏颜色为透明
+            ((Activity) context).getWindow().setStatusBarColor(Color.TRANSPARENT);
         }
     }
 }
