1db661144202cb62df0f3f46680311de2b8fb49c
panda102 <1565675791@qq.com>
2018-02-08 23:43:48 +0800

    修改LogUtil工具

--- app/src/main/java/com/nxm/muzi102/utils/LogUtil.java
+++ app/src/main/java/com/nxm/muzi102/utils/LogUtil.java
@@ -8,14 +8,42 @@ import android.util.Log;
  * 2017年12月18日22:43:48              lzx                                          Log工具
  */
 public class LogUtil {
+    public static final int VERBOSE = 1;
+    public static final int DEBUG = 2;
+    public static final int INFO = 3;
+    public static final int WARN = 4;
+    public static final int ERROR = 5;
+    public static final int NOTHING = 6;
+    public static int level = VERBOSE;
 
-    public static void e(String tag, String name) {
-        Log.e(tag, name);
-    }   public static void e(String tag, String name,Throwable tr) {
-        Log.e(tag, name,tr);
+
+    public static void v(String tag, String msg) {
+        if (level <= VERBOSE)
+            Log.d(tag, msg);
     }
 
-    public static void d(String tag, String name) {
-        Log.d(tag, name);
+    public static void d(String tag, String msg) {
+        if (level <= DEBUG)
+            Log.d(tag, msg);
+    }
+
+    public static void i(String tag, String msg) {
+        if (level <= INFO)
+            Log.d(tag, msg);
+    }
+
+    public static void w(String tag, String msg) {
+        if (level <= WARN)
+            Log.d(tag, msg);
+    }
+
+    public static void e(String tag, String msg) {
+        if (level <= ERROR)
+            Log.e(tag, msg);
+    }
+
+    public static void e(String tag, String msg, Throwable tr) {
+        if (level <= ERROR)
+            Log.e(tag, msg, tr);
     }
 }
